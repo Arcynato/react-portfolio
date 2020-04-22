0659e0c1f72acc4346d2d1a8d8247b98a4d07712
Arcynato <james.hostetter64@gmail.com>
2020-04-22 14:09:56 -0400

    Updated form component

---
+++ src/components/portfolio/portfolio-form.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class PortfolioForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            description: "",
+            category: "",
+            position: "",
+            url: "",
+            thumb_image: "",
+            banner_image: "",
+            logo: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.buildForm = this.buildForm.bind(this);
+    }
+
+    buildForm() {
+        let formData = new FormData();
+
+        formData.append("portfolio_item[name]", this.state.name);
+        formData.append("portfolio_item[description]", this.state.description);
+        formData.append("portfolio_item[url]", this.state.url);
+        formData.append("portfolio_item[category]", this.state.category);
+        formData.append("portfolio_item[position]", this.state.position);
+
+        return formData;
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        axios.post("https://jameshostetter.devcamp.space/portfolio/portfolio_items", this.buildForm(), { withCredentials: true })
+        .then(response => {
+            console.log("response", response);
+        }).catch(error => {
+            console.log("portfolio form handleSubmit error", error);
+        })
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Portfolio form goes here...</h1>
+
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <input type="text"
+                            name="name"
+                            placeholder="Portfolio Item Name"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+
+                        <input type="text"
+                            name="url"
+                            placeholder="URL"
+                            value={this.state.url}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <div>
+                        <input type="text"
+                            name="position"
+                            placeholder="Position"
+                            value={this.state.position}
+                            onChange={this.handleChange}
+                        />
+
+                        <select
+                            name="category"
+                            value={this.state.category}
+                            onChange={this.handleChange}
+                        >
+                            <option value="construction">Construction</option>
+                            <option value="help_desk">Help Desk</option>
+                            <option value="cable_technician">Cable Technician</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <textarea type="text"
+                            name="description"
+                            placeholder="Description"
+                            value={this.state.description}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <div>
+                        <button type="submit">Save</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -3,15 +3,16 @@ import React from 'react';
 const PortfolioSidebarList = props => {
     const PortfolioList = props.data.map(portfolioItem => {
         return (
-            <div className="portfolio-item-thumb">
+            <div key={portfolioItem.id} className="portfolio-item-thumb">
                 <div className="portfolio-thumb-img">
                     <img src={portfolioItem.thumb_image_url} />
                 </div>
                 <h1 className="title">{portfolioItem.name}</h1>
                 <h2>{portfolioItem.id}</h2>
             </div>
-        )
-    })
+        );
+    });
+
   return (
     <div className="portfolio-sidebar-list-wrapper">
       {PortfolioList}
