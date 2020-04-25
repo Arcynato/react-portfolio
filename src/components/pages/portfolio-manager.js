c1a9d31a70775ae8e331591d414b400da154a7ed
Arcynato <james.hostetter64@gmail.com>
2020-04-24 23:21:24 -0400

    Added delete functionality and api call

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -14,6 +14,7 @@ export default class PortfolioManager extends Component {
 
         this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
         this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
     }
 
     handleSuccessfulFormSubmission(portfolioItem) {
@@ -36,6 +37,22 @@ export default class PortfolioManager extends Component {
         })
     }
 
+    handleDeleteClick(portfolioItem) {
+        axios.delete(`https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`, { withCredentials: true })
+        .then(response => {
+            this.setState({
+                portfolioItems: this.state.portfolioItems.filter(item => {
+                    return item.id !== portfolioItem.id;
+                })
+            })
+
+            return response.data;
+        })
+        .catch(error => {
+            console.log("handleDeleteClick error", error);
+        });
+    }
+
     componentDidMount() {
         this.getPortfolioItems();
     }
@@ -51,7 +68,9 @@ export default class PortfolioManager extends Component {
                 </div>
 
                 <div className="right-column">
-                    <PortfolioSidebarList data={this.state.portfolioItems} />
+                    <PortfolioSidebarList
+                    handleDeleteClick={this.handleDeleteClick}
+                    data={this.state.portfolioItems} />
                 </div>
             </div>
         )
