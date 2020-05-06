0ee20c37a2180c145ac0bc76867d1f1a69bca7fd
Arcynato <james.hostetter64@gmail.com>
2020-05-06 00:35:32 -0400

    Updated code base.  Fixed memory leak when closing modal and submitting blog post to API.

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -4,77 +4,94 @@ import axios from 'axios';
 import RichTextEditor from '../forms/rich-text-editor';
 
 export default class BlogForm extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            title: "",
-            blog_status: "",
-        }
+    this.state = {
+      title: '',
+      blog_status: '',
+      content: '',
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.buildForm = this.buildForm.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.buildForm = this.buildForm.bind(this);
+    this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(
+      this
+    );
+  }
 
-    buildForm() {
-        let formData = new FormData();
+  buildForm() {
+    let formData = new FormData();
 
-        formData.append("portfolio_blog[title]", this.state.title);
-        formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+    formData.append('portfolio_blog[title]', this.state.title);
+    formData.append('portfolio_blog[blog_status]', this.state.blog_status);
+    formData.append('portfolio_blog[content]', this.state.content);
 
-        return formData;
-    }
+    return formData;
+  }
 
-    handleChange(event) {
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit() {
+    axios
+      .post(
+        'https://jameshostetter.devcamp.space/portfolio/portfolio_blogs',
+        this.buildForm(),
+        { withCredentials: true }
+      )
+      .then((response) => {
         this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+          title: '',
+          blog_status: '',
+        });
 
-    handleSubmit() {
-        axios.post("https://jameshostetter.devcamp.space/portfolio/portfolio_blogs", this.buildForm(), { withCredentials: true })
-        .then(response => {
-            this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
-            this.setState({
-                title: "",
-                blog_status: ""
-            })
-        })
-        .catch(error => {
-            console.log("error submitting blog form", error);
-        })
+        this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
+      })
+      .catch((error) => {
+        console.log('error submitting blog form', error);
+      });
 
-        event.preventDefault();
-    }
+    event.preventDefault();
+  }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} className="blog-form-wrapper">
-                <div className="two-column">
-                    <input
-                    onChange={this.handleChange}
-                    type="text"
-                    name="title"
-                    placeholder="Blog Title"
-                    value={this.state.title}
-                    />
+  handleRichTextEditorChange(content) {
+    this.setState({ content });
+  }
 
-                    <input
-                    onChange={this.handleChange}
-                    type="text"
-                    name="blog_status"
-                    placeholder="Blog Status"
-                    value={this.state.blog_status}
-                    />
-                </div>
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="blog-form-wrapper">
+        <div className="two-column">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name="title"
+            placeholder="Blog Title"
+            value={this.state.title}
+          />
 
-                <div className="one-column">
-                    <RichTextEditor />
-                </div>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name="blog_status"
+            placeholder="Blog Status"
+            value={this.state.blog_status}
+          />
+        </div>
 
-                <button>Save</button>
-            </form>
-        )
-    }
+        <div className="one-column">
+          <RichTextEditor
+            handleRichTextEditorChange={this.handleRichTextEditorChange}
+          />
+        </div>
+
+        <button>Save</button>
+      </form>
+    );
+  }
 }
