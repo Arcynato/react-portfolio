b75149bb7e7cd31c17ebaa8f5bc2df78d85992de
Arcynato <james.hostetter64@gmail.com>
2020-05-18 00:22:57 -0400

    Finalized login page styles and considered app finalized for deployment.

--- src/helpers/icon.js
+++ src/helpers/icon.js
@@ -8,6 +8,7 @@ import {
   faPlusCircle,
   faPhone,
   faMapMarkedAlt,
+  faLock,
 } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
@@ -21,7 +22,8 @@ const Icons = () => {
     faSpinner,
     faPlusCircle,
     faPhone,
-    faMapMarkedAlt
+    faMapMarkedAlt,
+    faLock
   );
 };
 
